15e5988d9be693cd6fe7b14c486dd29304b8f977
Damon Thomas <damon22t@gmail.com>
2025-06-19 01:10:47 -0400

    refactor: update Summary component layout; improve website and source code link presentation

--- src/components/project/Summary.tsx
+++ src/components/project/Summary.tsx
@@ -14,71 +14,60 @@ export default function Summary({
       <p className="text-base md:text-lg text-gray-700">
         {projectData?.expandedDescription}
       </p>
-      <div className="grid grid-cols-3 gap-2 mt-6">
-        <div className="flex flex-col gap-4">
-          <p className="font-extrabold">Website</p>
-          <div className="flex flex-col gap-2">
-            {projectData?.website ? (
-              <>
+      <div className="grid grid-cols-[min-content_1fr] gap-8 mt-6">
+        <p className="font-extrabold">Website</p>
+        <div className="flex  gap-6">
+          {projectData?.website ? (
+            <>
+              <Link
+                href={projectData.website}
+                target="_blank"
+                className={`font-extrabold text-lg md:text-xl text-[#835c00]  md:font-extrabold   hover:underline`}
+              >
+                Visit Website
+              </Link>
+              {projectData?.github ? (
                 <Link
-                  href={projectData.website}
+                  href={projectData.github}
                   target="_blank"
-                  className={`font-bold md:font-extrabold text-base md:text-lg hover:underline`}
-                  style={{ color: "#DE7833" }}
+                  className={`font-extrabold text-lg md:text-xl text-[#835c00] md:font-extrabold  hover:underline`}
                 >
-                  Visit Website
+                  Source Code
                 </Link>
-                {projectData?.github ? (
-                  <Link
-                    href={projectData.github}
-                    target="_blank"
-                    className={`font-bold md:font-extrabold text-base md:text-lg hover:underline`}
-                    style={{ color: "#DE7833" }}
-                  >
-                    Source Code
-                  </Link>
-                ) : (
-                  <span className="text-gray-500 text-sm md:text-base">
-                    No source code available
-                  </span>
-                )}
-              </>
-            ) : (
-              <span className="text-gray-500">No website available</span>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col items-center md:items-start">
-          <div className="w-fit flex flex-col gap-4">
-            {" "}
-            <p className="font-extrabold">Stack</p>
-            <div className="flex flex-col">
-              {projectData?.stack ? (
-                projectData.stack.map((tech, index) => (
-                  <p
-                    key={index}
-                    className="text-gray-800 font-medium md:font-bold text-base md:text-lg"
-                  >
-                    {tech}
-                  </p>
-                ))
               ) : (
-                <span className="text-gray-500">No stack information</span>
+                <span className="text-gray-500 text-sm md:text-base">
+                  No source code available
+                </span>
               )}
-            </div>
-          </div>
+            </>
+          ) : (
+            <span className="text-gray-500">No website available</span>
+          )}
         </div>
-        <div className="flex flex-col items-center md:items-start">
-          <div className="w-fit flex flex-col gap-4">
-            <p className="font-extrabold">Type</p>
-            <div className="flex flex-col gap-2">
-              {projectData?.type && (
-                <p className="text-gray-800 font-medium md:font-bold text-base md:text-lg">
-                  {projectData.type}
-                </p>
-              )}
-            </div>
-          </div>
+
+        <p className="font-extrabold">Stack</p>
+        <div className="flex flex-wrap gap-x-6">
+          {projectData?.stack ? (
+            projectData.stack.map((tech, index) => (
+              <p
+                key={index}
+                className="text-gray-800 font-medium md:font-bold text-base md:text-lg"
+              >
+                {tech}
+              </p>
+            ))
+          ) : (
+            <span className="text-gray-500">No stack information</span>
+          )}
+        </div>
+
+        <p className="font-extrabold">Type</p>
+        <div className="flex  gap-2">
+          {projectData?.type && (
+            <p className="text-gray-800 font-medium md:font-bold text-base md:text-lg">
+              {projectData.type}
+            </p>
+          )}
         </div>
       </div>
     </div>
