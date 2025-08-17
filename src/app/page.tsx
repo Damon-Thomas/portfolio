460554aa865daa9173823ed42397281a6a6fb1fb
Damon Thomas <damon22t@gmail.com>
2025-08-16 22:56:17 -0400

    Animation changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import CustomEase from "gsap/CustomEase";
 
 export default function Home() {
   const container = useRef<HTMLDivElement>(null);
+  const cover = useRef<HTMLDivElement>(null);
   const [currentPage, setCurrentPage] = useState<PageKey>("page1");
 
   function changePage() {
@@ -19,19 +20,75 @@ export default function Home() {
   function animateContainer() {
     if (container.current) {
       const height = container.current.offsetHeight;
-      const offset = 100; // adjust as needed for "a little over"
+      const offset = 0; // adjust as needed for "a little over"
       const transformOrigin = `-${height + offset}px 50% `;
 
-      gsap.to(container.current, {
-        rotation: "-=360",
-        duration: 2,
-        ease: "power2.inOut",
+      // gsap.to(container.current, {
+      //   rotation: "-=360",
+      //   duration: 1,
+      //   ease: "power2.inOut",
+      //   transformOrigin,
+      // });
+
+      //     gsap.to(container.current, {
+      //       rotation: "-=360",
+      //       duration: 1,
+      //       ease: "back.in.out(4)",
+      //       transformOrigin,
+      //     });
+
+      //     setTimeout(() => {
+      //       changePage();
+      //     }, 500);
+      //   }
+      // }
+
+      // Create a timeline for complex animation
+      const tl = gsap.timeline();
+      //fix animation
+      tl.to(container.current, {
+        rotation: "+=180",
+        duration: 0.5,
+        ease: "power2.in.out",
         transformOrigin,
-      });
+      })
+        .to(
+          cover.current,
+          {
+            rotation: "+=180",
+            duration: 0.5,
+            ease: "power2.in.out",
+            transformOrigin,
+          },
+          "<"
+        )
+        .to(container.current, {
+          rotation: "+=180",
+          duration: 0.5,
+          ease: "power2.in",
+          transformOrigin,
+        })
+        .to(cover.current, {
+          rotation: "-=180",
+          duration: 0.5,
+          ease: "power2.in",
+          transformOrigin,
+        });
+      // .to(cover.current, {
+      //   y: "+100%",
+      //   duration: 0.3,
+      //   ease: "power2.in",
+      // });
+      // .to(container.current, {
+      //   rotation: "+=3",
+      //   duration: 0.5,
+      //   ease: "bounce.out",
+      //   transformOrigin,
+      // });
 
       setTimeout(() => {
         changePage();
-      }, 1000);
+      }, 500); // adjusted timing for longer animation
     }
   }
 
@@ -45,14 +102,17 @@ export default function Home() {
   type PageKey = keyof typeof pages;
 
   useGSAP(() => {
-    // if (container.current) {
-    //   gsap.to(container.current, {
-    //     rotation: 360,
-    //     duration: 4,
-    //     ease: "linear",
-    //     transformOrigin: "500px 500px", // distance from the element’s own top-left
-    //   });
-    // }
+    if (cover.current) {
+      const height = cover.current.offsetHeight;
+      const offset = 100; // adjust as needed for "a little over"
+      const transformOrigin = `-${height + offset}px 50% `;
+      gsap.to(cover.current, {
+        rotation: "-=180",
+        duration: 1,
+        ease: "linear",
+        transformOrigin: transformOrigin,
+      });
+    }
   });
 
   return (
@@ -61,7 +121,17 @@ export default function Home() {
       style={{ padding: "32px" }}
     >
       <div className="bg-neutral-900 h-full w-full rounded-3xl relative overflow-hidden">
-        <div className="inner-shadow absolute  inset-0 z-10 w-full h-full pointer-events-none"></div>
+        <div
+          className="inner-shadow absolute  inset-0 z-20 w-full h-full pointer-events-none"
+          style={{
+            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.5'/%3E%3C/svg%3E")`,
+            mixBlendMode: "multiply",
+          }}
+        ></div>
+        <div
+          ref={cover}
+          className="cover absolute w-full h-full z-10 bg-neutral-900"
+        ></div>
         <div
           className="flex h-full w-full rounded-4xl text-black z-0"
           ref={container}
